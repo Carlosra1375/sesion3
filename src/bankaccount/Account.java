020e2629545d543504bc9f61bb3dde461c0c1fcf
carlo <carlo@DESKTOP-0MDM9UE>
2021-11-12 09:58:04 +0100

    1º commit: crear cuenta con métodos vacios

---
+++ src/bankaccount/Account.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bankaccount;
+
+/**
+ *
+ * @author carlo
+ */
+public class Account {
+    Double balance;
+
+    public Account(double balance){
+        this.balance = balance;
+    }
+    
+    public double withdraw(double money){
+        return 0;
+    }
+    
+    public boolean deposit(double money){
+        return false;
+    }
+}
